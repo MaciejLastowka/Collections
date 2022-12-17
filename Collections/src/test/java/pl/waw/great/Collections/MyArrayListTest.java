06bfdde11be97032c7a8486b20f919a657ec6159
MaciejLastowka <120501693+MaciejLastowka@users.noreply.github.com>
2022-12-17 14:20:57 +0100

    Add files via upload

---
+++ Collections/src/test/java/pl/waw/great/Collections/MyArrayListTest.java
@@ -0,0 +1,85 @@
+package pl.waw.great.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+
+    @Test
+    void add() {
+        MyArrayList list = new MyArrayList();
+
+        list.add("Bułka");
+        list.add("Chleb");
+
+        list.size();
+        assertEquals(2, list.size());
+
+    }
+
+    @Test
+    void get() {
+        MyArrayList list = new MyArrayList();
+        list.add("Bułka");
+        list.add("Chleb");
+        String s = (list.get(0));
+        assertEquals("Bułka", s );
+    }
+
+    @Test
+    void set() {
+        MyArrayList list = new MyArrayList();
+        list.add("Bułka");
+        list.add("Chleb");
+
+        String Chleb = list.set(1, "BURAK");
+        assertEquals("Chleb",Chleb);
+        assertEquals(list.get(1), "BURAK");
+
+    }
+
+    @Test
+    void remove() {
+        MyArrayList list = new MyArrayList();
+        list.add("Bułka");
+        list.add("Chleb");
+        list.add("Masło");
+
+        String remove = list.remove(0);
+
+        assertEquals("Bułka", remove);
+        assertEquals(list.get(0), "Chleb");
+        assertEquals(list.get(1), "Masło");
+    }
+
+    @Test
+    void testRemove() {
+        MyArrayList list = new MyArrayList();
+        list.add("Bułka");
+        list.add("Chleb");
+        list.add("Masło");
+
+        assertEquals(0,"Bułka");
+
+    }
+
+    @Test
+    void indexOf() {
+        MyArrayList list = new MyArrayList();
+        list.add("Bułka");
+        list.add("Chleb");
+        list.add("Masło");
+
+        boolean isRemoved = list.remove("Bułka");
+        assertTrue(isRemoved);
+
+        assertEquals(list.get(0), "Chleb");
+        assertEquals(list.get(1), "Masło");
+
+    }
+}
+
+
